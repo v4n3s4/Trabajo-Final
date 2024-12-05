907c152b008d0bb01bae5148d561e27d24c218ba
vanesa <doravanesa333@gmail.com>
2024-12-05 01:28:19 -0300

    Delete entrega 3 directory

--- entrega 3/ejer3.py
+++
@@ -1,9 +0,0 @@
-
-numeros = (1, 3, 5, 7, 3, 9, 3, 2, 3, 6, 8, 3)
-
-numero_buscado = int(input("Introduce un número para ver cuántas veces se repite en la tupla: "))
-
-contador = numeros.count(numero_buscado)
-
-
-print(f"El número {numero_buscado} se repite {contador} veces en la tupla.")
